72b0eda0040720da4c413c541ad70e3b5ff87154
Pavel Bychko <box@abordage.dev>
2022-09-16 23:49:43 +0300

    Add throw exception

--- src/Factory/PizzaSimpleFactory/SimplePizzaFactory.php
+++ src/Factory/PizzaSimpleFactory/SimplePizzaFactory.php
@@ -2,15 +2,21 @@
 
 namespace Pattern\Factory\PizzaSimpleFactory;
 
+use Exception;
+
 class SimplePizzaFactory
 {
+    /**
+     * @throws Exception
+     */
     public function createPizza(string $type): Pizza
     {
         return match ($type) {
             'cheese' => new CheesePizza(),
             'clam' => new ClamPizza(),
             'veggie' => new VeggiePizza(),
-            default => new PepperoniPizza(),
+            'pepperoni' => new PepperoniPizza(),
+            default => throw new Exception('invalid type of pizza'),
         };
     }
 }
